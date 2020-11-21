70a9159f4dd58a9f77f6fe9271a2c7624e599abe
olivervorasai <olivervorasai@gmail.com>
2020-11-20 17:32:58 -0800

    Index: Condense brand logos in loop, fixed stylings

--- pages/index.vue
+++ pages/index.vue
@@ -12,78 +12,27 @@
           md="8"
           lg="4"
         >
-          <h1 class="text-h4 mb-4 black--text">ATC Autobody &amp; Paint</h1>
+          <h1 class="text-h4 mb-4 blue--text">ATC Autobody &amp; Paint</h1>
           <h2 class="text-h4 black--text">
             Owner operated collision repair center in the heart of Portland.
           </h2>
         </v-col>
       </v-row>
     </v-parallax>
-    <v-row justify="center">
+    <v-row class="my-12" justify="center">
       <v-img
+        v-for="image in images"
+        :key="image.pathShort"
         :max-height="brandHeight"
         :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/toyota-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/ford-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/honda-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/kia-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/nissan-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/chevrolet-logo.jpg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/subaru-logo.png')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/volkswagon-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/hyundai-logo.svg')"
-        contain
-      ></v-img>
-      <v-img
-        :max-height="brandHeight"
-        :max-width="brandHeight"
-        :src="require('@/assets/brand-logos/mazda-logo.svg')"
+        :src="image.pathLong"
+        class="mx-12 my-6"
         contain
       ></v-img>
     </v-row>
-    <v-row no-gutters>
-      <v-col class="pa-12">
-        <h1 class="text-center mb-8">
+    <v-row>
+      <v-col cols="6" class="pa-16 blue lighten-5">
+        <h1 class="text-center blue--text mb-8">
           ATC Auto Body is a Full-Service Auto Body Repair Shop
         </h1>
         <p class="text-center text-h5 mb-8">
@@ -101,7 +50,7 @@
           reputation for high quality repairs.
         </p>
       </v-col>
-      <v-col>
+      <v-col cols="6" class="pa-0">
         <v-img
           :src="require('@/assets/stock-images/front-image-1.jpg')"
         ></v-img>
@@ -115,7 +64,20 @@ export default {
   data() {
     return {
       brandHeight: 150,
+      images: [],
     }
+  },
+  mounted() {
+    this.importAll(
+      require.context('@/assets/brand-logos/', true, /\.svg$|.png$|.jpg$/)
+    )
+  },
+  methods: {
+    importAll(r) {
+      r.keys().forEach((key) =>
+        this.images.push({ pathLong: r(key), pathShort: key })
+      )
+    },
   },
 }
 </script>
@@ -123,8 +85,5 @@ export default {
 <style scoped>
 .hero-box {
   background-color: rgb(255, 255, 255, 0.75);
-}
-.car-logos > * {
-  display: inline-block;
 }
 </style>
