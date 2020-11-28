ea042b504811f9371a7d5ecccdf1bf37d3718c8e
olivervorasai <olivervorasai@gmail.com>
2020-11-28 00:54:06 -0800

    Index: Added services section, modified index columns for mobile, modified image sizes

--- pages/index.vue
+++ pages/index.vue
@@ -31,40 +31,90 @@
       ></v-img>
     </v-row>
     <v-row>
-      <v-col cols="6" class="pa-16 blue lighten-5">
-        <h1 class="text-center blue--text mb-8">
+      <v-col cols="12" sm="12" md="6" lg="6" class="pa-16 blue lighten-5">
+        <h1 class="text-center text-h5 text-lg-h4 blue--text mb-4 mb-xl-12">
           ATC Auto Body is a Full-Service Auto Body Repair Shop
         </h1>
-        <p class="text-center text-h5 mb-8">
+        <p class="text-center text-h6 text-xl-h5 mb-xl-12">
           ATC Auto Body and Paint is an owner operated collision repair center
           in the heart of Portland. We offer superior and full repair services.
         </p>
-        <p class="text-center text-h5 mb-8">
+        <p class="text-center text-h6 text-xl-h5 mb-xl-12">
           ATC uses the best equipment and material for repairs as well as
           utilizing the most advanced technologies leaving our customers more
           than satisfied.
         </p>
-        <p class="text-center text-h5 mb-8">
+        <p class="text-center text-h6 text-xl-h5 mb-xl-12">
           Customer satisfaction is our number One priority, so we strive to have
           your vehicle in and out as fast as possible, while maintaining our
           reputation for high quality repairs.
         </p>
       </v-col>
-      <v-col cols="6" class="pa-0">
+      <v-col cols="12" sm="12" md="6" lg="6" class="pa-0">
         <v-img
           :src="require('@/assets/stock-images/front-image-1.jpg')"
+          height="100%"
         ></v-img>
+      </v-col>
+    </v-row>
+    <v-row class="mx-6 mx-md-16 mt-6">
+      <v-col cols="12">
+        <h1 class="text-center blue--text">
+          ATC Autobody &amp; Paint Services
+        </h1>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <div class="text-center mb-6">
+          <v-icon size="96" color="blue darken-2">{{ mdiShieldCar }}</v-icon>
+        </div>
+        <h2 class="text-h5 text-lg-h4 font-weight-bold mb-6">
+          Let us deal work with your insurance.
+        </h2>
+        <p class="text-h6">
+          Shopping for auto repairs can be stressful when your only mode of
+          transporation is broken down. Allow us to provide free estimates and
+          handle insurance claims worry free.
+        </p>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <div class="text-center mb-6">
+          <v-icon size="96" color="red">{{ mdiSpray }}</v-icon>
+        </div>
+        <h2 class="text-h5 text-lg-h4 font-weight-bold mb-6">
+          Restore your vehicle with expert painting.
+        </h2>
+        <p class="text-h6">
+          Our expert painting and color matching will make your car look as good
+          as new. We also provide auto detailing for customization needs.
+        </p>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="4">
+        <div class="text-center mb-6">
+          <v-icon size="96" color="green">{{ mdiHammerWrench }}</v-icon>
+        </div>
+        <h2 class="text-h5 text-lg-h4 font-weight-bold mb-6">
+          Fix any body damage from accidents to crashes.
+        </h2>
+        <p class="text-h6">
+          We excel at paintless dent removals, automotive glass replacement, and
+          uni-body repair and straightening.
+        </p>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import { mdiShieldCar, mdiSpray, mdiHammerWrench } from '@mdi/js'
+
 export default {
   data() {
     return {
       brandHeight: 150,
       images: [],
+      mdiShieldCar,
+      mdiSpray,
+      mdiHammerWrench,
     }
   },
   mounted() {
